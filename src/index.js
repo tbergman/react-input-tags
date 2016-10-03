ed707ce776f880b67aea87c70b92caa5f9e09da8
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-10-03 11:50:11 -0400

    babel transpiles es2015 javascript to es5

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+var React = require('react');
+var ReactDOM.render(
+    <h1>Hello, world!</h1>
+    document.getElementById('example')
+    );
+*/
+
+[1, 2, 3].map((item) => {
+  console.log(item);
+});
