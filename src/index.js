9159127ca67ab4769d25167f20f663c56fe7706c
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-10-03 11:56:46 -0400

    browserify with react

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ var ReactDOM.render(
 [1, 2, 3].map((item) => {
   console.log(item);
 });
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world</h1>,
+  document.getElementById('example')
+);
